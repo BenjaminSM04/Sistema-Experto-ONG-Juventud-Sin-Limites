c9e7c2c99b02490644a739714ba5fc403515e054
BenjaminSM04 <Benjaminspsn@outlook.com>
2025-10-25 20:34:40 -0400

    cambios en comentarios

--- Domain/BI/MetricasProgramaMes.cs
+++ Domain/BI/MetricasProgramaMes.cs
@@ -1,17 +1,17 @@
-using Sistema_Experto_ONG_Juventud_Sin_Limites.Domain.Common;
+﻿using Sistema_Experto_ONG_Juventud_Sin_Limites.Domain.Common;
 
 namespace Sistema_Experto_ONG_Juventud_Sin_Limites.Domain.BI;
 
 public class MetricasProgramaMes : BaseEntity
 {
     public int ProgramaId { get; set; }
-    public string AnioMes { get; set; } = string.Empty; // 'YYYY-MM'
+    public string AnioMes { get; set; } = string.Empty; // 
     public int ActividadesPlanificadas { get; set; }
     public int ActividadesEjecutadas { get; set; }
     public decimal PorcCumplimiento { get; set; }
     public decimal RetrasoPromedioDias { get; set; }
     public decimal PorcAsistenciaProm { get; set; }
 
-    // Navegaci�n
+    // Navegación
     public Programas.Programa Programa { get; set; } = null!;
 }
